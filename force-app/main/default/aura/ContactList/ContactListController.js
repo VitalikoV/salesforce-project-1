96d34768226971f57719e78536c03cc6e2d00e45
VitalikoV <drap877@gmail.com>
2020-12-16 13:58:49 +0200

    changed the logic of datatable

--- force-app/main/default/aura/ContactList/ContactListController.js
+++ force-app/main/default/aura/ContactList/ContactListController.js
@@ -2,5 +2,29 @@
 	doInit : function(component, event, helper) {
 		helper.setColumns(component);
         helper.queryContacts(component);
-	}
+    },
+    
+        //function invokes when user press Next button
+     handleNext : function(component, event, helper) { 
+        var pageNumber = component.get("v.pageNumber");
+        component.set("v.pageNumber", pageNumber+1);
+        helper.queryContacts(component);
+    },
+
+    //function invokes when user press Prev button
+    handlePrev : function(component, event, helper) {        
+        var pageNumber = component.get("v.pageNumber");
+        component.set("v.pageNumber", pageNumber-1);
+        helper.queryContacts(component);
+    }, 
+    
+    //retreive field, that required to sort and direction(asc, desc). Then invoke function that sorts data
+    updateColumnSorting : function(component, event, helper){
+        let fieldName = event.getParam('fieldName');
+        let sortDirection = event.getParam('sortDirection');
+        component.set("v.sortedBy", fieldName);
+        component.set("v.sortedDirection", sortDirection);
+        helper.sortData(component, fieldName, sortDirection);
+    }
+
 })
--- force-app/main/default/aura/ContactList/ContactListHelper.js
+++ force-app/main/default/aura/ContactList/ContactListHelper.js
@@ -1,18 +1,24 @@
 ({
+    // columns for datatable
 	setColumns : function(component) {
 		    component.set('v.columns', [
-            {label: 'Name', fieldName: 'linkName', type: 'url',
+                {label: 'Name', fieldName: 'linkName', type: 'url', sortable: "true",
              typeAttributes: {label: { fieldName: 'Name' },value:{fieldName: 'linkName'}, target: '_blank'}
 			},
-            {label: 'Email', fieldName: 'Email', type: 'email'},
-            {label: 'Phone', fieldName: 'Phone', type: 'phone'},
+                {label: 'Email', fieldName: 'Email', type: 'email', sortable: "true"},
+                {label: 'Phone', fieldName: 'Phone', type: 'phone', sortable: "true"},
         ]);
 	}, 
-                
+    
+    
     queryContacts : function(component) {
-        	var data = component.get("c.getRelatedContacts");
+        	let data = component.get("c.getRelatedContacts");
+            let pageSize = component.get("v.pageSize").toString();
+        	let pageNumber = component.get("v.pageNumber").toString(); 
             data.setParams({
-                recordId : component.get('v.recordId')
+                recordId : component.get('v.recordId'),
+                'pageSize' : pageSize,
+            	'pageNumber' : pageNumber
             });
             
             data.setCallback(this, function(response){
@@ -22,10 +28,37 @@
                 contacts.forEach(function(record){
                     record.linkName = '/'+record.Id;
                 });
+                if(contacts.length < component.get("v.pageSize")){
+                    component.set("v.isLastPage", true);
+                } else{
+                    component.set("v.isLastPage", false);
+                }
+				component.set("v.dataSize", contacts.length); 
                 component.set("v.contacts", contacts);
+                sortData(component, component.get("v.sortedBy"), component.get("v.sortedDirection"));
                 }
             });
             
             $A.enqueueAction(data);
+    },
+
+
+    
+    //sort method, that changes order of contacts in the table
+    sortData : function(component, fieldName, sortDirection){
+        let data = component.get("v.contacts");
+        let reverse = sortDirection !== 'asc';
+        data.sort(this.sortBy(fieldName, reverse));
+        component.set("v.contacts", data)
+    },
+    
+    //sort logic
+     sortBy: function (field, reverse) {
+        var key = function(x) {return x[field]};
+        reverse = !reverse ? 1 : -1;
+        return function (a, b) {
+            return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+        }
     }
+
 })
