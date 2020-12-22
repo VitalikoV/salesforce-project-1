893a77f279c6310866edea12b3fb843a7d875bef
VitalikoV <drap877@gmail.com>
2020-12-22 12:03:29 +0200

    changed weather component and added small version

--- force-app/main/default/aura/WeatherComponent/WeatherComponentController.js
+++ force-app/main/default/aura/WeatherComponent/WeatherComponentController.js
@@ -1,6 +1,6 @@
-({
-	doInit : function(component, event, helper) {
-		let data = component.get('c.getWeather');
+({  
+    doInit : function(component, event, helper) {
+		let data = component.get('c.getAllWeather');
         data.setParams({
             recordId: component.get('v.recordId')
         });
@@ -10,11 +10,12 @@
             let state = response.getState();
             console.log(weather);
             if(state === 'SUCCESS'){
-                component.set("v.weather", weather);
-                component.set("v.icon", "http://openweathermap.org/img/w/" + weather.icon + ".png")
+                component.set("v.main", weather.main);
+                component.set("v.weather", weather.weather[0]);
+                component.set("v.city", weather.name);
+                component.set("v.icon", "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png");
             }
         });
-        
         $A.enqueueAction(data);
 	}
 })
---
+++ force-app/main/default/aura/WeatherComponentSmall/WeatherComponentSmallController.js
@@ -0,0 +1,19 @@
+({
+	doInit : function(component, event, helper) {
+		let data = component.get('c.getWeather');
+        data.setParams({
+            recordId: component.get('v.recordId')
+        });
+        
+        data.setCallback(this, function(response){
+            let weather = response.getReturnValue();
+            let state = response.getState();
+            console.log(weather);
+            if(state === 'SUCCESS'){
+                component.set("v.weather", weather);
+                component.set("v.icon", "http://openweathermap.org/img/w/" + weather.icon + ".png")
+            }
+        });
+        $A.enqueueAction(data);
+    }
+})
